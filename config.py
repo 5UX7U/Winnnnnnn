40f7de471b46f4c5408affa2f689c2a689862ead
5UX7U <125146407+5UX7U@users.noreply.github.com>
2023-04-29 01:22:37 +0200

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5527406116:AAFwTTILY1ad3aCpbfvedlRkZohf4kFSIDM")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5527406116:AAGM1dPjGBxEaiFM2WwXHjF-k30V7adAreQ")
 SUDO = int(os.environ.get("SUDO", "5004499890"))
-HEROKU = os.environ.get("HEROKU", "")
+HEROKU = os.environ.get("HEROKU", "kdbdjdbfjffb")
 APP_URL = "https://"+ HEROKU +".herokuapp.com/" + BOT_TOKEN
